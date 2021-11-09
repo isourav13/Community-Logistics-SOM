ad6dcc416638ec8729deae70bf66a8b95868d9a9
SD <isourav13@gmail.com>
2021-11-09 18:32:03 +0000

    added figures

--- src/SOM.py
+++ src/SOM.py
@@ -9,15 +9,23 @@ Created on Thu Sep 30 08:41:14 2021
 from numpy.random import RandomState
 import pandas as pd
 import numpy as np
-from minisom import MiniSom, TestMinisom
+from minisom import MiniSom
 import matplotlib.pyplot as plt
-%matplotlib inline
+from sklearn.cluster import KMeans
+import geopandas as gpd
+
+from shapely.geometry import Point, Polygon
+import contextily as cx
 
 
 ############################   Data Loading and Randomizing weight column w.r.t. Sales per person  ##################################
 
-df = pd.read_csv('/Users/isourav/Documents SSD/Assignment/Dissertation/Dataset/Input.csv')
+df = pd.read_csv('/Users/isourav/Documents SSD/Assignment/Dissertation/Dataset/Input_Data.csv')
 df['Weight'] = np.random.randint(1, 5, df.shape[0])*df.iloc[:, 1]
+
+
+
+
 
 
 
@@ -38,7 +46,8 @@ test_data = test_data.values
 
 
 
-som_shape = (1, 6)
+
+som_shape = (1, 4)
 som = MiniSom(som_shape[0], som_shape[1], train_data.shape[1], sigma=.5, learning_rate=.5,
               neighborhood_function='gaussian', activation_distance='euclidean', random_seed=10)
 
@@ -67,10 +76,10 @@ font1 = {'family':'serif','color':'black','size':20}
 font2 = {'family':'serif','color':'darkred','size':15}
 
 
-plt.figure(figsize=(10, 10))
+plt.figure(figsize=(6, 6))
 for c in np.unique(cluster_index):
-    plt.scatter(test_data[cluster_index == c, 0],
-                test_data[cluster_index == c, 1], label='Cluster='+str(c+1), alpha=.7)
+    plt.scatter(test_data[cluster_index == c, 3],
+                test_data[cluster_index == c, 4], label='Cluster='+str(c+1), alpha=.7)
 
 # plotting centroids
 for centroid in som.get_weights():
@@ -84,6 +93,31 @@ plt.ylabel("Longitude", fontdict = font2)
 
 
 
+
+
+
+
+
+
+
+nyc = gpd.read_file('/Users/isourav/Documents SSD/Assignment/Dissertation/Dataset/Shape/geo_export_900c7e2d-c78c-44d4-8d41-4ee68d2d5b9f.shp')
+df_wm=nyc.to_crs(epsg=4326)
+crs = {'init':'EPSG:4326'}
+geometry = [Point(xy) for xy in zip(df['Longitude'], df['Latitude'])]
+geo_df = gpd.GeoDataFrame(df, 
+                          crs = crs, 
+                          geometry = geometry)
+
+
+ax = df_wm.plot(figsize=(10, 10), alpha=0.3, edgecolor='BLACK', linewidth=3)
+ax.set_title('New York City')
+cx.add_basemap(ax, crs=df_wm.crs.to_string(), source=cx.providers.Stamen.Watercolor, zoom=12)
+cx.add_basemap(ax, crs=df_wm.crs.to_string(), source=cx.providers.Stamen.TonerLabels, zoom=10)
+geo_df.plot(ax=ax,color='BLUE',linewidth=0.4)
+
+
+
+
 ############################   TSP   ##################################
 
 
@@ -91,16 +125,9 @@ np.random.RandomState(10)
 N_points = 12
 N_neurons = N_points*2
 t = np.linspace(0, np.pi*2, N_points)
-
 x= test_data[0:20,3]
 y= test_data[0:20,4]
-
-
-#x = np.cos(t)+(np.random.rand(N_points)-.5)*.3
-#y = np.sin(t)+(np.random.rand(N_points)-.5)*.3
-
 som = MiniSom(som_shape[0], N_neurons, 2, sigma=2, learning_rate=.4, neighborhood_function='gaussian', random_seed=0)
-
 points = np.array([x,y]).T
 som.random_weights_init(points)
 
@@ -123,5 +150,20 @@ for i, iterations in enumerate(range(10, 601, 50)):
 
 plt.tight_layout()
 plt.show()
+
+
+############################   K-means    ##################################
+
+kmeans = KMeans(n_clusters=4, algorithm="elkan").fit(test)
+centroids = kmeans.cluster_centers_
+inertia = kmeans.inertia_ #Sum of squared distances of samples to their closest cluster center, weighted by the sample weights if provided.
+
+print(centroids)
+plt.figure(figsize=(10,10))
+plt.scatter(test['Latitude'], test['Longitude'], c= kmeans.labels_.astype(float), s=50, alpha=0.5, linewidths=0.2)
+plt.scatter(centroids[:, 3], centroids[:, 4], c='red', s=200, marker='x')
+plt.show()
+
+
 
 
